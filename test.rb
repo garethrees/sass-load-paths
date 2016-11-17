0994c5c8bdfcc1ac48bc221da9bb503f70031d6b
Gareth Rees <gareth@garethrees.co.uk>
2016-11-17 12:07:29 +0000

    Add tests for supplying different filename options

--- test.rb
+++ test.rb
@@ -22,4 +22,61 @@ class TestSassPaths < Test::Unit::TestCase
             'scss output does not match custom_theme'
   end
 
+
+  def test_simple_override_with_correct_filename_option
+    paths = %w(theme core).map do |dir|
+      File.expand_path(File.dirname(__FILE__) + "/stylesheets/#{ dir }")
+    end
+
+    all_path =
+      File.expand_path(File.dirname(__FILE__) + '/stylesheets/core/all.scss')
+
+    custom_path =
+      File.expand_path(File.dirname(__FILE__) + '/stylesheets/theme/custom.scss')
+
+    options = { :load_paths => paths,
+                :syntax => :scss,
+                :filename => custom_path,
+                :original_filename => all_path }
+
+    template =
+      File.read(
+        File.expand_path(
+          File.dirname(__FILE__) + '/stylesheets/core/all.scss'))
+
+    engine = Sass::Engine.new(template, options)
+
+    assert engine.render =~ /custom_theme/,
+            'scss output does not match custom_theme'
+  end
+
+  def test_simple_override_with_incorrect_filename_option
+    paths = %w(theme core).map do |dir|
+      File.expand_path(File.dirname(__FILE__) + "/stylesheets/#{ dir }")
+    end
+
+    all_path =
+      File.expand_path(File.dirname(__FILE__) + '/stylesheets/core/all.scss')
+
+    custom_path =
+      File.expand_path(File.dirname(__FILE__) + '/stylesheets/core/custom.scss')
+
+    options = { :load_paths => paths,
+                :syntax => :scss,
+                :filename => custom_path,
+                :original_filename => all_path }
+
+    template =
+      File.read(
+        File.expand_path(
+          File.dirname(__FILE__) + '/stylesheets/core/all.scss'))
+
+    engine = Sass::Engine.new(template, options)
+
+    # Given both filename options point to the core dir, the rendered css should
+    # NOT match the theme override
+    assert engine.render !=~ /custom_theme/,
+            'scss output matches custom_theme'
+  end
+
 end
